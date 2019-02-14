4992df260894f3db09e0299b4bba86eb12a68005
Tina Chen <xuachen@microsoft.com>
2019-02-14 14:27:56 -0800

    Initial commit

---
+++ app/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace app
+{
+    public class Program
+    {
+        /// <summary>
+        /// Some field.
+        /// </summary>
+        int field = 3;
+
+        /// <summary>
+        /// My console app's entrypoint.
+        /// </summary>
+        /// <param name="args">Command line arguments.</param>
+        static void Main(string[] args)
+        {
+            new Program().AnotherMethod();
+        }
+
+        void AnotherMethod()
+        {
+            string local = "somevalue" + field;
+            string anotherLocal = local;
+
+            string finalOne = string.Join(local, anotherLocal);
+        }
+    }
+}
